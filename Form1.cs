e8a3e63f8391edb1c501206b3b3949a4385a07ae
stingdau <closer.pham@gmail.com>
2018-01-25 09:06:11 +0700

    Button tru

--- Form1.cs
+++ Form1.cs
@@ -23,5 +23,12 @@ namespace Buoi2
             double c = a + b;
             txtKetQua.Text = c.ToString();
         }
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a - b;
+            txtKetQua.Text = c.ToString();
+        }
     }
 }
