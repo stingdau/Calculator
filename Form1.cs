2415a0de3d25a23a0eff40cad293d9b8cf6d3544
stingdau <closer.pham@gmail.com>
2018-01-25 08:48:14 +0700

    Fix button

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,12 @@ namespace Buoi2
             InitializeComponent();
         }
 
-  
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtA.Text);
+            double b = double.Parse(txtB.Text);
+            double c = a + b;
+            txtKetQua.Text = c.ToString();
+        }
     }
 }
